f22b90d42b2b63ffa149d77885995d4daf0d1fef
iresh <ireshmw@gmail.com>
2016-12-31 21:14:04 +0530

    Added tomcat compatible war file making functionality

--- src/main/java/com/iresh/ContactAppApplication.java
+++ src/main/java/com/iresh/ContactAppApplication.java
@@ -2,11 +2,18 @@ package com.iresh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ContactAppApplication {
+public class ContactAppApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ContactAppApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ContactAppApplication.class);
 	}
 }
