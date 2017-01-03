c2a0bab8185f0df2c3510b00443fc3504d9c40ad
iresh <ireshmw@gmail.com>
2017-01-03 12:47:18 +0530

    renamed few files and separated contact delete jquery code section to new js file.

--- src/main/resources/static/js/contact-adding-button-action.js
+++ src/main/resources/static/js/contact-adding-button-action.js
@@ -4,10 +4,6 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
-    var csrfToken = $('#_csrf').attr("content");
-    var csrfHeader = $('#_csrf_header').attr("content");
-
-
     // $.ajaxSetup({ headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') } });
 
     // $.ajaxSetup({
@@ -55,37 +51,5 @@ $( document ).ready(function() {
         // });
 
     });
-    $("#table-body").on('click', '.delete-record', function (event) {
-        event.preventDefault();
-        var recId = $(this).parent().siblings(".rec-id").html();
-        var rec = $(this).parent().siblings(".rec-id");
-
-        console.log(recId);
-        console.log("click the button...");;;;;;;;;;;
-        $.ajax({
-            method:'POST',
-            url:'/delete',
-            data: JSON.stringify ({id:recId}),
-            beforeSend: function(xhr){
-                xhr.setRequestHeader(csrfHeader, csrfToken);},
-            dataType : 'json',
-            contentType:'application/json',
-            success:function(data){
-                if (data["state"] == 1){
-                    console.log("inside the if statement...");
-                    console.log(data["state"]);
-                    rec.parent().remove();
-
-                }
-            },
-            error:function(error){
-                console.log(error);
-            }
-        });
-        event.preventDefault();
-    });
-
-
-
 
 });
